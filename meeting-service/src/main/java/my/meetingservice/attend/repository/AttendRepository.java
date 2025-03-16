3dc3c5c97833d83348a69d40a1f8d07ccea692a4
preej <preejer8013@gmail.com>
2025-03-16 17:13:04 +0900

    commit

--- meeting-service/src/main/java/my/meetingservice/attend/repository/AttendRepository.java
+++
@@ -1,7 +0,0 @@
-package my.meetingservice.attend.repository;
-
-import my.meetingservice.attend.Attend;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AttendRepository extends JpaRepository<Attend,Long> {
-}
